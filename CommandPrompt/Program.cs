0fc65e87e67a11466604abecc45e819449ce17c6
gmullins925 <50452654+gmullins925@users.noreply.github.com>
2019-08-19 13:49:37 -0500

    Initial Commit

---
+++ CommandPrompt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPrompt
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CommandPrompt myCommand = new CommandPrompt(10, 15);
+			myCommand.SetScreenText(2, "This line 2 text");
+			myCommand.SetScreenText(1, "This is line 2 text");
+			myCommand.SetScreenText(4, "At line 4");
+			myCommand.Display();
+			Console.ReadLine();
+		}
+	}
+}
